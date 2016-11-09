6bac49ea51635b4f744c409538a9741c922dd2a2
isa1374 <isha13gp@hotmail.com>
2016-11-09 12:49:05 -0600

    eclipse again

--- segunda-parte/src/Principal.java
+++ segunda-parte/src/Principal.java
@@ -1,38 +1,30 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 
-public class Principal{
-  public static void Main(String [] args)
-  {
 
-    String [] NoTerminales=null;
-    String [] Terminales=null;
-    String [] Inicial= null;
-    ArrayList <String> Prod = new ArrayList <String>();
-    try {
-      File file=new File("C:/Users/Rogger/Desktop/mates/Gramatica/gram.txt");
-      FileReader read=new FileReader(file);
-      BufferedReader buff= new BufferedReader(read);
-      LineNumberReader num= new LineNumberReader(buff);
 
-      String line="";
+public class Principal {
 
-      while(line!=null)
-      {
-        line=num.readLine();
-        NoTerminales=line.split(",");
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String [] NoTerminal = null; 
+		String [] Terminal = null; 
+		String inicial = ""; 
+		ArrayList<String> Prod = new ArrayList<String>(); 
+		
+		File file = new File ("C:/Users/isha1_000/Documents/GitHub/SegundaParteMC/segunda-parte/src/Gram.txt");
+		FileReader rd = new FileReader(file);
+		BufferedReader bf = new BufferedReader(rd); 
+		LineNumberReader ln = new LineNumberReader(rd);
+		
+		String line=""; 
+		
+		
+	}
 
-        if(num.getLineNumber>3)
-        {
-          String Cadena=num.read
-          Prod.add(Regla())
-        }
-
-      }
-    }
-  }
 }
