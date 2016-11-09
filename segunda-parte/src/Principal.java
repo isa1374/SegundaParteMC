3e222232ad4b42b833a023bece8b56383d6b88a0
isa1374 <isha13gp@hotmail.com>
2016-11-09 13:38:58 -0600

    lectura exitosa

--- segunda-parte/src/Principal.java
+++ segunda-parte/src/Principal.java
@@ -1,47 +1,53 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 
-
-
 public class Principal {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		
 		String [] NoTerminal = null; 
 		String [] Terminal = null; 
 		String inicial = ""; 
 		ArrayList<String> Prod = new ArrayList<String>(); 
 		
-		File file = new File ("C:/Users/isha1_000/Documents/GitHub/SegundaParteMC/segunda-parte/src/Gram.txt");
-		FileReader rd = new FileReader(file);
-		BufferedReader bf = new BufferedReader(rd); 
-		LineNumberReader ln = new LineNumberReader(rd);
-		
-		String line=""; 
-		
-		//Read and save lines
-		while(line!=null){
-			switch(ln.getLineNumber()){
-			case 0: 
-					line=ln.readLine(); 
-					NoTerminal=line.split(",");
-					
-					break; 
-			default: 
-					break; 
+		try{
+			File file = new File ("C:/Users/isha1_000/Documents/GitHub/SegundaParteMC/segunda-parte/src/Gram.txt");
+			FileReader rd = new FileReader(file);
+			LineNumberReader ln = new LineNumberReader(rd);
+			
+			String line=""; 
+			
+			//Read and save lines
+			while(line !=null){
+				switch(ln.getLineNumber()){
+				case 0: 
+						line=ln.readLine(); 
+						NoTerminal=line.split(",");
+						break; 
+				case 1: 
+						line = ln.readLine(); 
+						Terminal = line.split(",");
+						break;
+				case 2: 
+						line = ln.readLine(); 
+						inicial = line; 
+						break;
+				default: 
+						line = ln.readLine(); 
+						Prod.add(line);
+						break; 
+				}
 			};
-		};
-		
-		//Check of saved values 
-		for(int i=0; i<NoTerminal.length; i++){
-			System.out.println(NoTerminal[i].toString());
+			ln.close();
+			
+			
+		}catch( IOException e){
+			System.out.println("Error");
 		}
-		ln.close();
+			
 	}
 
 }
